4c53a21537148979cdeb039ffcd2acfd5c1a527b
Askaholic <askaholic907@gmail.com>
2018-11-10 14:39:00 -0900

    Refactored for motors in general

--- coffee.py
+++ coffee.py
@@ -1,23 +1,40 @@
 from RPi import GPIO
 from time import sleep
 
+
+class Motor(object):
+    """docstring for Motor."""
+    def __init__(self, pinA, pinB, pinE):
+        self.A = pinA
+        self.B = pinB
+        self.E = pinE
+
+        GPIO.setup(self.A, GPIO.OUT)
+        GPIO.setup(self.B, GPIO.OUT)
+        GPIO.setup(self.E, GPIO.OUT)
+
+    def forward(self):
+        GPIO.output(self.A, GPIO.HIGH)
+        GPIO.output(self.B, GPIO.LOW)
+        GPIO.output(self.E, GPIO.HIGH)
+
+    def stop(self):
+        GPIO.output(self.E, GPIO.LOW)
+
+
 GPIO.setmode(GPIO.BOARD)
 
 Motor1A = 16
 Motor1B = 18
 Motor1E = 22
 
-GPIO.setup(Motor1A, GPIO.OUT)
-GPIO.setup(Motor1B, GPIO.OUT)
-GPIO.setup(Motor1E, GPIO.OUT)
+motor = Motor(Motor1A, Motor1B, Motor1E)
 
 print("Turning motor on")
-GPIO.output(Motor1A, GPIO.HIGH)
-GPIO.output(Motor1B, GPIO.LOW)
-GPIO.output(Motor1E, GPIO.HIGH)
+motor.forward()
 sleep(2)
 
 print("Stopping motor")
-GPIO.output(Motor1E, GPIO.LOW)
+motor.stop()
 
 GPIO.cleanup()
